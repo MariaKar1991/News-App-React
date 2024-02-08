979b7e24f98757ad71f6a0617751da2c477c907c
Maria Karavazaki <mariaskaravazaki@gmail.com>
2024-02-08 15:21:51 +0000

    Update App.v2.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import NewsBoard from "./Components/NewsBoard";
 import { useState } from "react";
 
 const App = () => {
+  // State to manage the selected news category
   const [category, setCategory] = useState("general");
 
   return (
     <div>
+      {/* Navbar component with setCategory prop */}
       <Navbar setCategory={setCategory} />
+
+      {/* NewsBoard component with category prop */}
       <NewsBoard category={category} />
     </div>
   );
